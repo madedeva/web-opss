7ac07eeaf7d5421934e38bea8adfa9d73ad77fd8
madedeva <deva.kerti@undiksha.ac.id>
2024-07-26 23:40:24 +0800

    Menambah menu sidebar, merubah tampilan table

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -15,6 +15,8 @@ const Sidebar: React.FC<SidebarProps> = ({ isVisible }) => {
             <img src="/logo/opss-logo.png" alt="OPSS Logo" className="w-36"/>
         </div>
       <ul className="relative mt-4">
+
+        {/* role opeator */}
         <li className={`relative ${currentPath === '/dashboard' ? 'bg-orange-500 rounded' : ''}`}>
             <a href="/dashboard" className="flex items-center text-sm py-4 px-6 h-12 overflow-hidden text-white font-semibold text-ellipsis whitespace-nowrap rounded hover:text-white hover:bg-orange-500 transition duration-300 ease-in-out">
             <svg width="32px" height="32px" viewBox="-6 -6 36.00 36.00" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#ffffff"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M22 12.2039V13.725C22 17.6258 22 19.5763 20.8284 20.7881C19.6569 22 17.7712 22 14 22H10C6.22876 22 4.34315 22 3.17157 20.7881C2 19.5763 2 17.6258 2 13.725V12.2039C2 9.91549 2 8.77128 2.5192 7.82274C3.0384 6.87421 3.98695 6.28551 5.88403 5.10813L7.88403 3.86687C9.88939 2.62229 10.8921 2 12 2C13.1079 2 14.1106 2.62229 16.116 3.86687L18.116 5.10812C20.0131 6.28551 20.9616 6.87421 21.4808 7.82274" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path> <path d="M15 18H9" stroke="#ffffff" strokeWidth="1.5" strokeLinecap="round"></path> </g></svg>
@@ -61,6 +63,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isVisible }) => {
         </li>
       </ul>
 
+      {/* role author */}
       <hr />
       
       <ul>
@@ -83,6 +86,7 @@ const Sidebar: React.FC<SidebarProps> = ({ isVisible }) => {
         </li>
       </ul>
 
+      {/* role reviewer */}
       <hr />
 
       <ul>
