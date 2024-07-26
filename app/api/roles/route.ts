77e6809b90477fd25051c0e56a8f22f3b7af0a62
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2024-07-26 21:06:47 +0800

    Update Register

---
+++ app/api/roles/route.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+  try {
+    const roles = await prisma.role.findMany();
+    return NextResponse.json(roles);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+};
