c701abdf99691c6bc03435f6d9aa7dfba0446d46
madedeva <deva.kerti@undiksha.ac.id>
2024-07-26 17:03:05 +0800

    Menambahkan menu sidebar

--- app/dashboard/conference/page.tsx
+++ app/dashboard/conference/page.tsx
@@ -29,46 +29,52 @@ const Conference =  async () => {
     <DashboardLayout>
         <WelcomeCard />
 
-        <div className="mt-4">
-            <h1 className="text-lg font-semibold text-gray-800">Conference Page</h1>
-
-            <div className="bg-white rounded-lg overflow-hidden w-full mt-4">
-                <div className="mb-2 px-4 py-4">
-                    <AddConference/>
-                </div>
-
-                <table className="table mb-4">
-                    <thead className="text-sm text-black">
-                        <tr>
-                            <th>Number</th>
-                            <th>Title</th>
-                            <th>Start Date</th>
-                            <th>End Date</th>
-                            <th>Chair</th>
-                            <th>Location</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {conference.map((conference, index) => (
-                            <tr key={conference.id}>
-                                <td>{index + 1}</td>
-                                <td>{conference.name}</td>
-                                {/* date format dd mm yyyy */}
-                                <td>{new Date(conference.startDate).toLocaleDateString()}</td>
-                                <td>{new Date(conference.endDate).toLocaleDateString()}</td>
-                                <td>{conference.User?.name}</td>
-                                <td>{conference.address}</td>
-                                <td className="flex space-x-1">
-                                    <DeleteConference conference={conference}/>
-                                    <UpdateConference conference={conference}/>
-                                </td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+        <div className="bg-white p-6 rounded-lg mt-4">
+        <div className="mt-6">
+          <h3 className="text-lg font-medium">My Conferences</h3>
+          <p className="text-sm text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+          <hr className="mt-2"/>
+          <div className="mt-4">
+            <AddConference />
+          </div>
+          <table className="table-auto min-w-full bg-white mt-6">
+            <thead>
+              <tr>
+                <th className="py-2">Conference Name</th>
+                <th className="py-2">Conference Date</th>
+                <th className="py-2">Submission Deadline</th>
+                <th className="py-2">Statistic</th>
+                <th className="py-2">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+            {conference.map((conference, index) => (
+              <tr className="text-gray-700" key={conference.id}>
+                <td className="py-2">{conference.name}</td>
+                <td className="py-2">
+                  <p>Date Start: {new Date(conference.startDate).toLocaleDateString()}</p>
+                  <p>Date End: {new Date(conference.endDate).toLocaleDateString()}</p>
+                </td>
+                <td className="py-2">
+                    <p>Full Paper Submission</p>
+                    <p>January 26, 2024</p>
+                </td>
+                <td className="py-2">
+                  <p><a className="underline" href="#">Author Registered Authors - 10</a></p>
+                  <p><a className="underline" href="#">Paper Submitted Paper - 10</a></p>
+                </td>
+                <td className="py-2">
+                    <DeleteConference conference={conference}/>
+                    <UpdateConference conference={conference}/>
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     </DashboardLayout>
     )
 };
