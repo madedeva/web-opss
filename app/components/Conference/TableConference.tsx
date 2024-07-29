b0863a5c71223f582eb9c3cd7387680156163ceb
Gede Yuda Aditya <gdyudaaditya123@gmail.com>
2024-07-30 00:04:02 +0800

    Hanya mencaru data user yang login

---
+++ app/components/Conference/TableConference.tsx
@@ -0,0 +1,59 @@
+'use client'
+import DeleteConference from "@/app/dashboard/conference/deleteConference";
+import UpdateConference from "@/app/dashboard/conference/updateConference";
+import { Conference, PrismaClient, User } from "@prisma/client";
+import { useSession } from "next-auth/react";
+
+const TableConference = ({conference}:{ conference:  Conference[]}) => {
+
+    const { data: session } = useSession();
+
+    const user = session?.user as User;
+
+    const filteredConferences = conference.filter(conference => conference.userId === user?.id);
+
+    return (
+        <table className="table-auto min-w-full bg-white mt-6">
+        <thead>
+          <tr>
+            <th className="py-2">Conference Name</th>
+            <th className="py-2">Conference Date</th>
+            <th className="py-2">Submission Deadline</th>
+            <th className="py-2">Statistic</th>
+            <th className="py-2">Status</th>
+            <th className="py-2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+        {filteredConferences.map((conference) => (
+          <tr className="text-gray-700" key={conference.id}>
+            <td className="py-2">{conference.name}</td>
+            <td className="py-2">
+              <p>Date Start: {new Date(conference.startDate).toLocaleDateString()}</p>
+              <p>Date End: {new Date(conference.endDate).toLocaleDateString()}</p>
+            </td>
+            <td className="py-2">
+                <p>Full Paper Submission</p>
+                <p>{new Date(conference.submission_deadline).toLocaleDateString()}</p>
+            </td>
+            <td className="py-2">
+              <p><a className="underline" href="#">Author Registered Authors - 10</a></p>
+              <p><a className="underline" href="#">Paper Submitted Paper - 10</a></p>
+            </td>
+            <td className="py-2">
+              <p className={conference.status === 'Active' ? 'text-green-500' : 'text-red-500'}>
+                  {conference.status.charAt(0).toUpperCase() + conference.status.slice(1)}
+              </p>
+            </td>
+            <td className="py-2">
+                <DeleteConference conference={conference}/>
+                <UpdateConference conference={conference}/>
+            </td>
+          </tr>
+        ))}
+        </tbody>
+      </table>
+    )
+};
+
+export default TableConference 
