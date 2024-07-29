d3c4f54cb3d7e57a1ca782d4212944e2d89be872
madedeva <deva.kerti@undiksha.ac.id>
2024-07-29 21:47:11 +0800

    get conference by logged user

--- app/dashboard/_app.tsx
+++ app/dashboard/_app.tsx
@@ -1,8 +1,13 @@
 import { AppProps } from 'next/app';
 import '../styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SessionProvider session={pageProps.session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp;
