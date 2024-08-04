15da3265e0e998d1cda9c02aff2f745e7a6210b2
madedeva <deva.kerti@undiksha.ac.id>
2024-08-04 16:32:55 +0800

    update

--- app/components/MyConference/TableMyConference.tsx
+++ app/components/MyConference/TableMyConference.tsx
@@ -4,6 +4,25 @@ import UpdateConference from "@/app/dashboard/conference/updateConference";
 import { RegisterConference, User } from "@prisma/client";
 import { useSession } from "next-auth/react";
 
+const getOrdinalSuffix = (day: number) => {
+  if (day > 3 && day < 21) return 'th';
+  switch (day % 10) {
+      case 1: return 'st';
+      case 2: return 'nd';
+      case 3: return 'rd';
+      default: return 'th';
+  }
+};
+
+const getFormattedDate = (date: Date | string): string => {
+  const dateObj = typeof date === 'string' ? new Date(date) : date;
+  const day = dateObj.getDate();
+  const month = dateObj.toLocaleString('en-US', { month: 'long' });
+  const year = dateObj.getFullYear();
+  const suffix = getOrdinalSuffix(day);
+  return `${month} ${day}${suffix}, ${year}`;
+};
+
 type UserCon = {
   country: string,
   userId: number,
@@ -15,6 +34,10 @@ type UserCon = {
   conference: {
     id: number,
     name: string,
+    description: string,
+    submission_deadlineStart: Date
+    submission_deadlineEnd: Date
+    paper_template: string
       User: {
           name: string,
           email: string,
@@ -33,18 +56,18 @@ const TableMyConference = ({ reg_conference }: { reg_conference: UserCon[] }) =>
             <table className="min-w-full bg-white mt-6 text-left">
         <thead>
           <tr className="text-sm">
-            <th className="py-2 w-60">Conference Name</th>
-            <th className="py-2">Conference Date</th>
-            <th className="py-2">Submission Deadline</th>
-            <th className="py-2 w-60">Actions</th>
+            <th className="py-2">Conference Name</th>
+            <th className="py-2">Full Paper Submission</th>
+            <th className="py-2">Paper Template</th>
+            <th className="py-2">Actions</th>
           </tr>
         </thead>
         <tbody>
         {filteredRegConferences.map((reg_conference) => (
             <tr className="text-gray-700 text-sm text-left" key={reg_conference.id}>
               <td className="py-2">{reg_conference.conference.name}</td>
-              <td className="py-2">{reg_conference.user.name}</td>
-              <td className="py-2">{reg_conference.country}</td>
+              <td className="py-2">{getFormattedDate(reg_conference.conference.submission_deadlineStart)} - {getFormattedDate(reg_conference.conference.submission_deadlineEnd)}</td>
+              <td className="py-2">{reg_conference.conference.paper_template}</td>
               <td className="py-2">
                 {/* <UpdateConference reg_con={reg_con} />
                 <DeleteConference reg_con={reg_con} /> */}
@@ -53,7 +76,7 @@ const TableMyConference = ({ reg_conference }: { reg_conference: UserCon[] }) =>
           ))}
         </tbody>
       </table>
-        </div>
+      </div>
     );
 };
 
--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,13 +1,9 @@
-// import NextAuth from 'next-auth';
-// import { DefaultSession } from 'next-auth';
+// import NextAuth, { DefaultSession } from "next-auth";
 
-// declare module 'next-auth' {
+// declare module "next-auth" {
 //   interface Session {
 //     user: {
-//       name?: string | null;
-//       email?: string | null;
-//       image?: string | null;
-//       roleId?: string;
-//     };
+//       id: string;
+//     } & DefaultSession["user"];
 //   }
 // }
