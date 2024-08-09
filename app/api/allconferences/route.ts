7f0b694b95fbf1e39ce0c643466aa871e5be68dc
madedeva <deva.kerti@undiksha.ac.id>
2024-08-09 11:31:12 +0800

    Update

---
+++ app/api/allconferences/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const GET = async () => {
+    try {
+        const conferences = await prisma.conference.findMany({
+            where: {
+                status: 'Active',
+            },
+            take: 8,
+            select: {
+                id: true,
+                name: true,
+                startDate: true,
+                endDate: true,
+                submission_deadlineEnd: true,
+                submission_deadlineStart: true,
+                city: true,
+                country: true,
+                banner: true,
+                slug: true
+            },
+        }) 
+        return NextResponse.json(conferences); 
+    }catch (e){
+        console.error(e);
+        return NextResponse.json({ status: "fail", error: e });
+    }
+}
